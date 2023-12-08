265b213d2597608608b6cb3fc82526daf1808706
Renan Horta <renan.horta96@gmail.com>
2023-12-08 12:28:26 -0300

    [add] adicionado metodo para ler arquivo csv

---
+++ Models/Conta.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ContasBancaias_at.Models
+{
+    public class Conta
+    {
+        protected int numConta { get; set; }
+        protected string correntista { get; set; }
+        protected double saldo { get; set; }
+
+        public Conta() { }
+        public Conta(int numConta, string correntista, double saldo)
+        {
+            this.numConta = numConta;
+            this.correntista = correntista;
+            this.saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return correntista + "-" +numConta + " R$" + saldo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using ContasBancaias_at.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace ContasBancaias_at
 {
@@ -10,6 +10,60 @@ namespace ContasBancaias_at
     {
         static void Main(string[] args)
         {
+            List<Conta> conta = new List<Conta>();
+
+            conta = lerConta(conta);
+            exibirConta(conta);
+        }
+
+        public static List<Conta> lerConta(List<Conta> conta)
+        {   
+            const string localArquivo = @"C:\Users\Renan_PC\source\repos\ContasBancaias_at\utils\csv_conta.csv";
+            try
+            {
+                using (var arquivo = new StreamReader(localArquivo))
+                {
+                    bool primeiraLinha = true;
+                    string linha = arquivo.ReadLine();
+                    while (linha != null)
+                    {   
+                        if (!primeiraLinha)
+                        {
+                            try
+                            {
+                                // adicionar um try catch para as entradas numericas
+                                string[] campos = linha.Split(';');
+                                int numConta = int.Parse(campos[0]);
+                                string correntista = campos[1];
+                                double saldo = double.Parse(campos[2]);
+                                conta.Add(new Conta(numConta, correntista, saldo));
+                            } catch
+                                {
+                                Console.WriteLine("Erro nos dados dos arquivos.");
+                                }
+                        }
+                        else
+                        {
+                            //pula primeira linha
+                            primeiraLinha = false;
+                        }
+
+                        linha = arquivo.ReadLine();
+                    }
+                }
+
+            } catch (Exception ex){
+                Console.WriteLine("Erro na leitura do arquivo.");
+            }
+
+            return conta;
+        }
+        
+        public static void exibirConta(List<Conta> lista) { 
+            foreach (Conta conta in lista)
+            {
+                Console.WriteLine(conta);
+            }
         }
     }
 }
